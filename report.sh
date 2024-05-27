69d8de39c1f0c68710b78c84c81351946e54091e
Ryoga-exe <git@ryoga.dev>
2024-05-28 00:06:40 +0900

    feat: update script

--- report.sh
+++ report.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-STUDENT_NAME="筑波太郎"
-STUDENT_ID="202311XXX"
-STUDENT_UTID="s2311XXX"
+
+if [ -e .env ]; then
+    eval "$(cat .env <(echo) <(declare -x))"
+    [ -z "$STUDENT_NAME" ] && echo "WARN: STUDENT_NAME is empty"
+    [ -z "$STUDENT_ID" ] && echo "WARN: STUDENT_NAME is empty"
+    [ -z "$STUDENT_UTID" ] && echo "WARN: STUDENT_UTID is empty"
+else
+    STUDENT_NAME="筑波太郎"
+    STUDENT_ID="202311XXX"
+    STUDENT_UTID="s2311XXX"
+fi
+
 
 for file in $(find . -type f -name 'report*.raw.txt'); do
     sed -e "s/__STUDENT_NAME__/$STUDENT_NAME/g" -e "s/__STUDENT_ID__/$STUDENT_ID/g" -e "s/__STUDENT_UTID__/$STUDENT_UTID/g" $file \
